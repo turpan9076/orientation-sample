8f44fdbd5483889a1184be144411db1fcef989fc
turpan9076 <10x@mbr.nifty.com>
2023-10-16 16:41:12 +0900

    first commit

--- orientation.js
+++ orientation.js
@@ -46,7 +46,7 @@ function orientation(event) {
         // deviceorientationabsoluteイベントのalphaを補正
         degrees = compassHeading(alpha, beta, gamma);
     }
-    needle.style.transform = `rotate(${360-degrees}deg)`;
+    needle.style.transform = `rotate(${180-degrees}deg)`;
     
     let direction;
     if (
