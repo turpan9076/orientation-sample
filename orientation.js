1d83d7a3b275123711d34495dc4d3da9d4b57105
turpan9076 <10x@mbr.nifty.com>
2023-12-10 22:22:59 +0900

    first commit

--- orientation.js
+++ orientation.js
@@ -1,3 +1,34 @@
+// OS識別用
+let os;
+
+// DOM構築完了イベントハンドラ登録
+window.addEventListener("DOMContentLoaded", init);
+
+// 初期化
+function init() {
+    // 簡易的なOS判定
+    os = detectOSSimply();
+    if (os == "iphone") {
+        // safari用。DeviceOrientation APIの使用をユーザに許可して貰う
+        document.querySelector("#permit").addEventListener("click", permitDeviceOrientationForSafari);
+
+        window.addEventListener(
+            "deviceorientation",
+            orientation,
+            true
+        );
+    } else if (os == "android") {
+        window.addEventListener(
+            "deviceorientationabsolute",
+            orientation,
+            true
+        );
+    } else{
+        window.alert("Sample not supported by PC");
+    }
+}
+
+
 // ジャイロスコープと地磁気をセンサーから取得
 function orientation(event) {
     let absolute = event.absolute;
@@ -10,33 +41,16 @@ function orientation(event) {
     if(os == "iphone") {
         // webkitCompasssHeading値を採用
         degrees = event.webkitCompassHeading;
-    } else {
+
+    }else{
         // deviceorientationabsoluteイベントのalphaを補正
         degrees = compassHeading(alpha, beta, gamma);
     }
-
-    // 初回の角度
-    if (typeof orientation.lastDegree === 'undefined') {
-        orientation.lastDegree = degrees;
+    if (degrees > 0 && degrees < 180){
+        needle.style.transform = `rotate(${-(180+degrees)}deg)`;
+    }else if (degrees > 180 && degrees < 360){
+        needle.style.transform = `rotate(${180-degrees}deg)`;
     }
-
-    // 180度以上回転した場合は逆方向に回転させない
-    if (Math.abs(degrees - orientation.lastDegree) > 180) {
-        orientation.lastDegree = degrees > orientation.lastDegree ? degrees - 360 : degrees + 360;
-    }
-
-    // 針を滑らかに新しい角度に回転
-    needle.style.transform = `rotate(${degrees}deg)`;
-
-    // 最後の角度を更新
-    orientation.lastDegree = degrees;
-
-    // 方向の表示
-    updateDirection(degrees);
-}
-
-// 方向を表示する関数
-function updateDirection(degrees) {
     let direction;
     if (
         (degrees > 337.5 && degrees < 360) ||
@@ -67,3 +81,68 @@ function updateDirection(degrees) {
     document.querySelector("#gamma").innerHTML = gamma;
 }
 
+// 端末の傾き補正（Android用）
+// https://www.w3.org/TR/orientation-event/
+function compassHeading(alpha, beta, gamma) {
+    var degtorad = Math.PI / 180; // Degree-to-Radian conversion
+
+    var _x = beta ? beta * degtorad : 0; // beta value
+    var _y = gamma ? gamma * degtorad : 0; // gamma value
+    var _z = alpha ? alpha * degtorad : 0; // alpha value
+
+    var cX = Math.cos(_x);
+    var cY = Math.cos(_y);
+    var cZ = Math.cos(_z);
+    var sX = Math.sin(_x);
+    var sY = Math.sin(_y);
+    var sZ = Math.sin(_z);
+
+    // Calculate Vx and Vy components
+    var Vx = -cZ * sY - sZ * sX * cY;
+    var Vy = -sZ * sY + cZ * sX * cY;
+
+    // Calculate compass heading
+    var compassHeading = Math.atan(Vx / Vy);
+
+    // Convert compass heading to use whole unit circle
+    if (Vy < 0) {
+        compassHeading += Math.PI;
+    } else if (Vx < 0) {
+        compassHeading += 2 * Math.PI;
+    }
+
+    return compassHeading * (180 / Math.PI); // Compass Heading (in degrees)
+}
+
+// 簡易OS判定
+function detectOSSimply() {
+    let ret;
+    if (
+        navigator.userAgent.indexOf("iPhone") > 0 ||
+        navigator.userAgent.indexOf("iPad") > 0 ||
+        navigator.userAgent.indexOf("iPod") > 0
+    ) {
+        // iPad OS13のsafariはデフォルト「Macintosh」なので別途要対応
+        ret = "iphone";
+    } else if (navigator.userAgent.indexOf("Android") > 0) {
+        ret = "android";
+    } else {
+        ret = "pc";
+    }
+
+    return ret;
+}
+
+// iPhone + Safariの場合はDeviceOrientation APIの使用許可をユーザに求める
+function permitDeviceOrientationForSafari() {
+    DeviceOrientationEvent.requestPermission()
+        .then(response => {
+            if (response === "granted") {
+                window.addEventListener(
+                    "deviceorientation",
+                    detectDirection
+                );
+            }
+        })
+        .catch(console.error);
+}
